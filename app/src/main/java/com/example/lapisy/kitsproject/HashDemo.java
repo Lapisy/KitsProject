27733cab8c1fd8136201b4433868d4052b093781
lapisy <softgeek@163.com>
2018-10-10 20:47:48 +0800

    新建libarydemo，主要是完成一些常用库使用的demo

---
+++ app/src/main/java/com/example/lapisy/kitsproject/HashDemo.java
@@ -0,0 +1,9 @@
+package com.example.lapisy.kitsproject;
+
+public class HashDemo {
+    public static void main(String[] args) {
+        String name="java";
+        System.out.println(name.hashCode());
+        System.out.println(Integer.toBinaryString(name.hashCode()));
+    }
+}
--- app/src/main/java/com/example/lapisy/kitsproject/MainActivity.java
+++ app/src/main/java/com/example/lapisy/kitsproject/MainActivity.java
@@ -2,6 +2,21 @@ package com.example.lapisy.kitsproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Vector;
+import java.lang.String;
+
+import dalvik.system.PathClassLoader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +24,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("tag_data","onCreate");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("tag_data","onDestroy");
     }
 }
